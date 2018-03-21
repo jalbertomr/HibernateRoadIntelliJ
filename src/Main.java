5ef85bf2229f1157e518181db3547316b9d22d00
jalbertomr <jalbertomr@yahoo.com>
2018-03-21 16:12:20 -0600

    Primer Commit @Entity Persona mysql

---
+++ src/Main.java
@@ -0,0 +1,62 @@
+import org.bext.dto.Persona;
+import org.hibernate.HibernateException;
+import org.hibernate.Metamodel;
+import org.hibernate.query.Query;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import javax.persistence.metamodel.EntityType;
+
+import java.util.Map;
+
+public class Main {
+    private static final SessionFactory ourSessionFactory;
+
+    static {
+        try {
+            Configuration configuration = new Configuration();
+            configuration.configure();
+
+            ourSessionFactory = configuration.buildSessionFactory();
+        } catch (Throwable ex) {
+            throw new ExceptionInInitializerError(ex);
+        }
+    }
+
+    public static Session getSession() throws HibernateException {
+        return ourSessionFactory.openSession();
+    }
+
+    public static void main(final String[] args) throws Exception {
+        final Session session = getSession();
+        /*
+        try {
+
+            System.out.println("querying all the managed entities...");
+            final Metamodel metamodel = session.getSessionFactory().getMetamodel();
+            for (EntityType<?> entityType : metamodel.getEntities()) {
+                final String entityName = entityType.getName();
+                final Query query = session.createQuery("from " + entityName);
+                System.out.println("executing: " + query.getQueryString());
+                for (Object o : query.list()) {
+                    System.out.println("  " + o);
+                }
+            }
+        } finally {
+            session.close();
+        }
+        */
+        Persona persona = new Persona();
+        persona.setName("Primera Persona");
+        Persona persona2 = new Persona();
+        persona2.setName("Segunda Persona");
+
+        session.beginTransaction();
+        session.save(persona);
+        session.save(persona2);
+        session.getTransaction().commit();
+
+        session.close();
+    }
+}
